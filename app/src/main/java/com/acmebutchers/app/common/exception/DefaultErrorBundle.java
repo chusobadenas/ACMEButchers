8ae092dfc6cbf87e5c0a646f04a7e452a78e4ca8
Jesús Badenas <chusobadenas@gmail.com>
2017-01-25 13:24:01 +0100

    Creating class for handle exceptions

---
+++ app/src/main/java/com/acmebutchers/app/common/exception/DefaultErrorBundle.java
@@ -0,0 +1,48 @@
+package com.acmebutchers.app.common.exception;
+
+import android.content.Context;
+
+import com.acmebutchers.app.R;
+
+/**
+ * Wrapper around Exceptions used to manage default errors.
+ */
+public class DefaultErrorBundle implements ErrorBundle {
+
+  private Context context;
+  private Exception exception;
+  private Integer errorMsgId;
+
+  /**
+   * Constructor
+   *
+   * @param context    application context
+   * @param throwable  the exception
+   * @param errorMsgId resource id of the error message
+   */
+  public DefaultErrorBundle(Context context, Throwable throwable, Integer errorMsgId) {
+    this.context = context;
+    this.errorMsgId = errorMsgId;
+    if (throwable instanceof Exception) {
+      this.exception = (Exception) throwable;
+    }
+  }
+
+  @Override
+  public Exception getException() {
+    return exception;
+  }
+
+  @Override
+  public String getErrorMessage() {
+    String message;
+
+    if (errorMsgId == null) {
+      message = context.getString(R.string.exception_message_generic);
+    } else {
+      message = context.getString(errorMsgId);
+    }
+
+    return message;
+  }
+}
