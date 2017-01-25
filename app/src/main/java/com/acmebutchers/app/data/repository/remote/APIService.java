1c731ef4a7fe74ace4bfc6b6172b6248bf7dd442
Jesús Badenas <chusobadenas@gmail.com>
2017-01-25 16:57:24 +0100

    Flickr endpoint to get images

--- app/src/main/java/com/acmebutchers/app/data/repository/remote/APIService.java
+++ app/src/main/java/com/acmebutchers/app/data/repository/remote/APIService.java
@@ -1,5 +1,8 @@
 package com.acmebutchers.app.data.repository.remote;
 
+import android.support.annotation.NonNull;
+
+import com.acmebutchers.app.data.entity.response.Photos;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -9,13 +12,23 @@ import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rx.Observable;
 
 /**
  * APIService for retrieving data from the network using Retrofit.
  */
 public interface APIService {
 
-  String API_BASE_URL = "PUT YOUR API URL HERE";
+  String API_BASE_URL = "https://api.flickr.com/services/rest/";
+  String FLICKR_API_KEY = "a667caf5c8f62e77887300cb406d8701";
+
+  @GET
+  Observable<Photos> searchPhotos(@NonNull @Query("method") String method,
+                                  @NonNull @Query("api_key") String apiKey,
+                                  @Query("text") String text,
+                                  @Query("tags") String[] tags);
 
   /********
    * Helper class that sets up a new services
