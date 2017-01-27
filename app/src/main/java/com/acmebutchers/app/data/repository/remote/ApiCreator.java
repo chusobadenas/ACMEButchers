0bfdc37dcfcd3bf0581a7521716e2c39c07135a3
Jesús Badenas <chusobadenas@gmail.com>
2017-01-27 12:29:15 +0100

    Api creator in a new separated class

---
+++ app/src/main/java/com/acmebutchers/app/data/repository/remote/ApiCreator.java
@@ -0,0 +1,49 @@
+package com.acmebutchers.app.data.repository.remote;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public final class ApiCreator {
+
+  private ApiCreator() {
+    // Empty constructor
+  }
+
+  private static OkHttpClient createHttpClient() {
+    return new OkHttpClient.Builder()
+        .readTimeout(10000, TimeUnit.MILLISECONDS)
+        .connectTimeout(15000, TimeUnit.MILLISECONDS)
+        .build();
+  }
+
+  private static Retrofit createRetrofit(String baseUrl) {
+    Gson gson = new GsonBuilder()
+        .setDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+        .registerTypeAdapterFactory(GsonAdapterFactory.create())
+        .create();
+
+    return new Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .client(ApiCreator.createHttpClient())
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+        .build();
+  }
+
+  /**
+   * Creates a new {@link FlickrApiService}
+   *
+   * @param baseUrl Flicker base url
+   * @return a new {@link FlickrApiService}
+   */
+  public static FlickrApiService newFlickrApiService(String baseUrl) {
+    return createRetrofit(baseUrl).create(FlickrApiService.class);
+  }
+}
