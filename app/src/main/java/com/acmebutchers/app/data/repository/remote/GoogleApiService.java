203f9c22f2e84718a96404469ec046ad4284ad0b
Jesús Badenas <chusobadenas@gmail.com>
2017-01-27 13:49:24 +0100

    New service to access Google API

---
+++ app/src/main/java/com/acmebutchers/app/data/repository/remote/GoogleApiService.java
@@ -0,0 +1,24 @@
+package com.acmebutchers.app.data.repository.remote;
+
+import android.support.annotation.NonNull;
+
+import com.acmebutchers.app.data.entity.response.PlaceSearch;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rx.Observable;
+
+/**
+ * Google Api Service for retrieving data from the network using Retrofit.
+ */
+public interface GoogleApiService {
+
+  String API_BASE_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/";
+  String GOOGLE_API_KEY = "AIzaSyDUBOw9A8_klQuiGMHdMyMfmtUDXBBc8RM";
+
+  @GET("json")
+  Observable<PlaceSearch> searchPlacesByKeyword(@NonNull @Query("key") String apiKey,
+                                                @NonNull @Query("location") String location,
+                                                @NonNull @Query("radius") Integer radius,
+                                                @Query("keyword") String keyword);
+}
