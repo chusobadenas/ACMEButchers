181db991281ac03834595db31f56aab39b69aaeb
Jesús Badenas <chusobadenas@gmail.com>
2017-01-27 14:02:38 +0100

    MVP for the map

--- app/src/main/java/com/acmebutchers/app/presentation/map/MapMvpView.java
+++ app/src/main/java/com/acmebutchers/app/presentation/map/MapMvpView.java
@@ -1,7 +1,16 @@
 package com.acmebutchers.app.presentation.map;
 
+import com.acmebutchers.app.domain.Place;
 import com.acmebutchers.app.presentation.base.MvpView;
+
+import java.util.List;
 
 interface MapMvpView extends MvpView {
 
+  /**
+   * Shows nearby butcher shops
+   *
+   * @param shops the list of shops
+   */
+  void showButcherShops(List<Place> shops);
 }
