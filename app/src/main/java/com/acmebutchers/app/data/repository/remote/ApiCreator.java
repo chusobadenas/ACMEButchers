68f7275bddddef47867f1af421910376ecd614bf
Jesús Badenas <chusobadenas@gmail.com>
2017-01-30 12:13:52 +0100

    Using interceptor in DEBUG mode

--- app/src/main/java/com/acmebutchers/app/data/repository/remote/ApiCreator.java
+++ app/src/main/java/com/acmebutchers/app/data/repository/remote/ApiCreator.java
@@ -1,11 +1,13 @@
 package com.acmebutchers.app.data.repository.remote;
 
+import com.acmebutchers.app.BuildConfig;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -17,7 +19,16 @@ public final class ApiCreator {
   }
 
   private static OkHttpClient createHttpClient() {
-    return new OkHttpClient.Builder()
+    OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder();
+
+    // Enable logging
+    if (BuildConfig.DEBUG) {
+      HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+      interceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+      clientBuilder.addInterceptor(interceptor);
+    }
+
+    return clientBuilder
         .readTimeout(10000, TimeUnit.MILLISECONDS)
         .connectTimeout(15000, TimeUnit.MILLISECONDS)
         .build();
