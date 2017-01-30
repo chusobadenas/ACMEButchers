b7c13c6f772e57fa0dfeb169eb0129c611e2f109
Jesús Badenas <chusobadenas@gmail.com>
2017-01-30 17:54:49 +0100

    Initialize Twitter

--- app/src/main/java/com/acmebutchers/app/AndroidApplication.java
+++ app/src/main/java/com/acmebutchers/app/AndroidApplication.java
@@ -6,7 +6,11 @@ import com.acmebutchers.app.common.di.HasComponent;
 import com.acmebutchers.app.common.di.components.ApplicationComponent;
 import com.acmebutchers.app.common.di.components.DaggerApplicationComponent;
 import com.acmebutchers.app.common.di.modules.ApplicationModule;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.tweetui.TweetUi;
 
+import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 public class AndroidApplication extends Application implements HasComponent<ApplicationComponent> {
@@ -18,6 +22,7 @@ public class AndroidApplication extends Application implements HasComponent<Appl
     super.onCreate();
     initializeInjector();
     initializeTimber();
+    initializeTwitter();
   }
 
   @Override
@@ -35,5 +40,11 @@ public class AndroidApplication extends Application implements HasComponent<Appl
     if (BuildConfig.DEBUG) {
       Timber.plant(new Timber.DebugTree());
     }
+  }
+
+  protected void initializeTwitter() {
+    TwitterAuthConfig authConfig = new TwitterAuthConfig(BuildConfig.TWITTER_KEY, BuildConfig
+        .TWITTER_SECRET);
+    Fabric.with(this, new TwitterCore(authConfig), new TweetUi());
   }
 }
--- app/src/test/java/com/acmebutchers/app/AndroidApplicationTest.java
+++ app/src/test/java/com/acmebutchers/app/AndroidApplicationTest.java
@@ -1,4 +1,9 @@
 package com.acmebutchers.app;
 
 public class AndroidApplicationTest extends AndroidApplication {
+
+  @Override
+  protected void initializeTwitter() {
+    // Nothing to do
+  }
 }
