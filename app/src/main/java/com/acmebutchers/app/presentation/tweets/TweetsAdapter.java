ffc6e2b11e9d0ec9711d6a8f1ed7f9bd538f5406
Jesús Badenas <chusobadenas@gmail.com>
2017-01-31 12:31:58 +0100

    Creating custom tweet layout

---
+++ app/src/main/java/com/acmebutchers/app/presentation/tweets/TweetsAdapter.java
@@ -0,0 +1,85 @@
+package com.acmebutchers.app.presentation.tweets;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.acmebutchers.app.R;
+import com.acmebutchers.app.common.util.UIUtils;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import twitter4j.Status;
+
+public class TweetsAdapter extends BaseAdapter {
+
+  private final Context context;
+  private final List<Status> tweets;
+
+  public TweetsAdapter(Context context, List<Status> tweets) {
+    super();
+    this.context = context;
+    this.tweets = tweets;
+  }
+
+  @Override
+  public int getCount() {
+    return tweets.size();
+  }
+
+  @Override
+  public Status getItem(int position) {
+    return tweets.get(position);
+  }
+
+  @Override
+  public long getItemId(int position) {
+    return position;
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    // Reuse view
+    ViewHolder holder;
+    LinearLayout currentView = (LinearLayout) convertView;
+
+    if (currentView == null) {
+      currentView = (LinearLayout) LayoutInflater.from(context).inflate(R.layout.custom_tweet, null);
+      holder = new ViewHolder(currentView);
+      currentView.setTag(holder);
+    } else {
+      holder = (ViewHolder) currentView.getTag();
+    }
+
+    // Get item
+    Status tweet = getItem(position);
+
+    // Display elements
+    UIUtils.loadImageUrl(context, holder.avatarImageView, tweet.getUser().getProfileImageURL());
+    holder.nameTextView.setText(tweet.getUser().getScreenName());
+    holder.statusTextView.setText(tweet.getText());
+
+    return currentView;
+  }
+
+  class ViewHolder {
+
+    @BindView(R.id.tweet_avatar)
+    ImageView avatarImageView;
+    @BindView(R.id.tweet_name)
+    TextView nameTextView;
+    @BindView(R.id.tweet_status)
+    TextView statusTextView;
+
+    ViewHolder(View view) {
+      ButterKnife.bind(this, view);
+    }
+  }
+}
